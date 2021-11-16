4bf417b1b0d9d504584fb15079b1991eb86addfc
gilangginti <gilangfauzi25@gmail.com>
2021-11-16 15:29:11 +0700

    handlelogin, show password, handle invalid username or password response to toast

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,30 +1,108 @@
-import React, { Component, useState } from 'react';
-import Loading from '../config/GlobalFunc';
+import React, { Component, useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router";
+import { toast } from "react-toastify";
+import Loading, { postRequest } from "../config/GlobalFunc";
 
 export default function Login() {
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+  const [isLoading, setIsLoading] = useState(false)
+  const [passwordShow, setPasswordShow] = useState(false)
+  const [isDisabled, setIsDisabled] = useState(false)
+  const history = useHistory();
+  
+  
+  useEffect(() => {
+    if(localStorage.getItem('token')){
+      history.push('/')
+    }
+  }, [])
+
+  function togglePassword(){
+    setPasswordShow(!passwordShow)
+  }
+
+  async function handleLogin() {
+    var usernameRegex = /\S+@\S+\.\S+/;
+    if (username === "admin@bukapedia.com" && password === "admin123") {
+      localStorage.setItem("token", "admin");
+      setIsLoading(true)
+      setIsDisabled(true)
+      history.push("/admindashboard");
+    } else {
+      if (username.match(usernameRegex)) {
+        toast.error('enter the correct username')
+      } else {
+        setIsLoading(true)
+        setIsDisabled(true)
+        await postRequest("auth/login", {
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
+          username: username,
+          password: password,
+        }).then((res) => {
+          if (res.data.token) {
+            localStorage.setItem("token", res.data.token);
+            history.push("/");
+          } else {
+            setIsLoading(false)
+            setIsDisabled(false)
+            const msgError = res.data.msg;
+            toast.error(msgError)
+          }
+        });
+      }
+    }
+    
+  }
   return (
     <div className="align-items-center justify-content-center vh-100 d-flex">
       <div className="card-container p-4">
         <h4>Selamat Datang</h4>
         <div className="mb-3 mt-3">
-          <label for="username" className="form-label">Username</label>
-          <input type="email" className="form-control" placeholder="Jhon123" id="username" />
+          <label for="username" className="form-label">
+            Username
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            placeholder="Jhon123"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
         </div>
         <div className="mb-3">
-          <label for="password" className="form-label">Password</label>
+          <label for="password" className="form-label">
+            Password
+          </label>
           <div className="d-flex flex-row">
-            <input type="password" className="form-control" placeholder="******" id="password" />
-            <button className="col-md-2 btn-eye">
-              <i className="fas fa-eye"></i>
-              {/* <i className="fas fa-eye-slash"></i> */}
+            <input
+              type={passwordShow ? "text" : 'password'}
+              className="form-control"
+              placeholder="******"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button className="col-md-2 btn-eye" onClick={togglePassword} >
+              {passwordShow ? <i className="fas fa-eye"/> : <i className="fas fa-eye-slash"/>}
             </button>
           </div>
         </div>
-        <button type="submit" class="btn btn-dark w-100 mt-3" disabled>
-          <Loading />
+        <button
+          type="submit"
+          class="btn btn-dark w-100 mt-3"
+          onClick={handleLogin}
+          disabled={isDisabled}
+        >
+          {isLoading ? <Loading /> : null}
           Login
         </button>
       </div>
     </div>
-  )
+  );
 }
