4858c5eb10e9c296ff06ab2619a45379271ba6e1
gilangginti <gilangfauzi25@gmail.com>
2021-11-17 12:18:22 +0700

    auth admin page. Just admin can access

--- src/pages/AdminDashboard.js
+++ src/pages/AdminDashboard.js
@@ -1,6 +1,16 @@
-import React from 'react'
+import React , {useEffect}from 'react'
+import { useSelector } from 'react-redux'
+import { useHistory } from 'react-router'
+import { loginUser, userSelector } from '../redux/slice/userSlice'
 
 const AdminDashboard = () => {
+    const history = useHistory()
+    const value = useSelector(userSelector)
+    useEffect(() => {
+        if(value !== 'admin'){
+            history.push('*')
+        }
+    }, [])
     return (
         <div>
             <h1>Admin Dashboard</h1>
