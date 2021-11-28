26db761037ed0509aa25e0d65170578195325139
Hanmustofa <32809284+hanmustofa@users.noreply.github.com>
2021-11-28 22:05:50 +0700

    add data table

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,9 +1,15 @@
+// import { iteratorSymbol } from "@reduxjs/toolkit/node_modules/immer/dist/internal";
 import React, { useEffect, useState } from "react";
 
 export default function Cart() {
-    
-    const [itemQuantity, setItemQuantity] = useState(1);
+    const [dummy] = useState ([{
+        title: "Acer SB220Q bi 21.5 inches Full HD (1920 x 1080) IPS Ultra-Thin",
+        price: 599,
+        quantity: 1,
+        totalprice: 599,
+    }])
 
+    
     return (
         <div className="bg-cartpage">
             <div className="container">
@@ -12,13 +18,23 @@ export default function Cart() {
                </div>
                 <div>
                     <table border="1" cellpadding="20" >
-                        <thead>
+                        <thead>                      
                         <tr>
                             <th>Product Name</th>
                             <th>Unit Price</th>
                             <th>Quantity</th>
                             <th>Total Price</th>
                         </tr>
+                        </thead>
+                        <thead>
+                        {dummy.map((item) => (
+                            <tr>
+                                <th>{item.title}</th>
+                                <th>{item.price}</th>
+                                <th>{item.quantity}</th>
+                                <th>{item.totalprice}</th>
+                            </tr>
+                        ))}
 
                         </thead>
                     </table>
